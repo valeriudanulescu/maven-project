11b694ce349f783ed6c3fef00564afba88f84f0e
valeriudanulescu <valeriudanulescu@gmail.com>
2018-08-03 16:49:36 +0300

    Added a newline at the end of Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,8 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-
-
+  
+ /**
+ *@param someone the name of the person
+ *@return greeting string
+ */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
